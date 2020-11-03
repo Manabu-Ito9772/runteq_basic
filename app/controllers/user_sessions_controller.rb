90fa73943f4813b6323006038c675173dbdf972b
Manabu Ito <m.ito09017@gmail.com>
2020-11-03 14:57:47 +0900

    フラッシュの追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,14 +4,17 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
+      flash[:success] = 'ログインしました'
       redirect_back_or_to root_url
     else
+      flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
 
   def destroy
     logout if logged_in?
+    flash[:success] = 'ログアウトしました'
     redirect_to root_url
   end
 end
