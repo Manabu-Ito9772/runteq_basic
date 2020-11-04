3e98b02fbae236d441598efbc940e54733e7b6fa
Manabu Ito <m.ito09017@gmail.com>
2020-11-04 13:33:12 +0900

    掲示板一覧実装

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,5 @@ class Board < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :title, presence: true, length: { maximum: 255 }
-  validates :body, presence: true, length: { maximum: 65535 }
-  validates :user_id, presence: true
+  validates :body, presence: true, length: { maximum: 65_535 }
 end
