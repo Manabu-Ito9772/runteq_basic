5e7d762fe63e397f4136ad2351e984e197690fcc
Manabu Ito <m.ito09017@gmail.com>
2020-11-06 16:05:35 +0900

    タイトルの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(name = '')
+    base_title = 'RUNTEQ BOARD APP'
+    if name.empty?
+      base_title
+    else
+      name + ' | ' + base_title
+    end
+  end
 end
