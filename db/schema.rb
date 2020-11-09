c18fdcc8dc55dff670fe612e3d900bd7e51ac0f2
Manabu Ito <m.ito09017@gmail.com>
2020-11-09 11:10:23 +0900

    ブックマークボタンのajax化

--- db/schema.rb
+++ db/schema.rb
@@ -23,18 +23,19 @@ ActiveRecord::Schema.define(version: 2020_11_07_234312) do
   end
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "board_id", null: false
+    t.integer "user_id"
+    t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["board_id"], name: "index_bookmarks_on_board_id"
+    t.index ["user_id", "board_id"], name: "index_bookmarks_on_user_id_and_board_id", unique: true
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "user_id", null: false
-    t.integer "board_id", null: false
+    t.integer "user_id"
+    t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["board_id"], name: "index_comments_on_board_id"
