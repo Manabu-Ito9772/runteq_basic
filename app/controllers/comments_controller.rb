7a603f2dc41c548ec202dbc0464626a49ee24c6a
Manabu Ito <m.ito09017@gmail.com>
2020-11-10 13:55:05 +0900

    コメント機能のajax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,32 @@
 class CommentsController < ApplicationController
+  before_action :require_login
+
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to board_path(comment.board), success: t('.success')
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def update
+    @comment = current_user.comments.find(params[:id])
+    if @comment.update(comment_update_params)
+      render json: { comment: @comment }, status: :ok
     else
-      redirect_to board_path(comment.board), danger: t('.danger')
+      render json: { comment: @comment, errors: { messages: @comment.errors.full_messages } }, status: :bad_request
     end
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
+
+  def comment_update_params
+    params.require(:comment).permit(:body)
   end
 end
